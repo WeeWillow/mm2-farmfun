6833440a79b73a802ea261b953fce8d4745dda7f
JacobBFynbo <151468827+JacobBFynbo@users.noreply.github.com>
2024-06-10 20:33:26 +0200

    Aktiviteter lavet
    Burde være færdig men billederne er stadig ikke helt gode

---
+++ assets/js/aktiviteter.js
@@ -0,0 +1,49 @@
+const urlBase = "https://api.jacobfynbo.com/wp-json/wp/v2/";
+const categorySlug = "forlystelser";
+const perPage = 20;
+
+const fetchPosts = () => {
+  fetch(`${urlBase}categories?slug=${categorySlug}`)
+    .then((res) => res.json())
+    .then((categories) => {
+      if (categories.length > 0) {
+        const categoryId = categories[0].id;
+
+        fetch(`${urlBase}posts?categories=${categoryId}&per_page=${perPage}`)
+          .then((res) => res.json())
+          .then((data) => {
+            displayPosts(data);
+          })
+          .catch((err) => console.error("Error fetching posts:", err));
+      }
+    });
+};
+
+const displayPosts = (posts) => {
+  const activityCards = document.querySelector(".activityCards");
+  activityCards.innerHTML = "";
+  posts.forEach((post) => {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const link = document.createElement("a");
+
+    link.href = post.link;
+
+    img.src = post.acf.billede.url;
+    img.alt = post.title.rendered;
+
+    h2.innerHTML = post.title.rendered;
+
+    link.appendChild(img);
+    link.appendChild(h2);
+
+    article.appendChild(link);
+
+    activityCards.appendChild(article);
+
+    console.log(post);
+  });
+};
+
+fetchPosts();
