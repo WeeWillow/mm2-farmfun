2ed0fbc390bd903c139e9f387578fa7c82145d84
JacobBFynbo <151468827+JacobBFynbo@users.noreply.github.com>
2024-06-13 20:57:47 +0200

    Opdateret carousel

---
+++ assets/js/indexForside.js
@@ -0,0 +1,32 @@
+let billedeIndex = 0;
+
+function visSlide(index) {
+  const slideGalleri = document.querySelector(".slideGalleri");
+  const slides = slideGalleri.querySelectorAll("img");
+  const antalSlides = slides.length;
+
+  // Håndter wrap-around
+  if (index >= antalSlides) {
+    billedeIndex = 0;
+  } else if (index < 0) {
+    billedeIndex = antalSlides - 1;
+  } else {
+    billedeIndex = index;
+  }
+
+  // Opdater containerens bredde
+  const offset = -billedeIndex * slides[0].clientWidth;
+  slideGalleri.style.transform = `translateX(${offset}px)`;
+}
+
+function tilbageSlider() {
+  visSlide(billedeIndex - 1);
+}
+
+function fremSlider() {
+  visSlide(billedeIndex + 1);
+}
+document.querySelector(".tilbage").addEventListener("click", tilbageSlider);
+document.querySelector(".frem").addEventListener("click", fremSlider);
+
+visSlide(billedeIndex);
